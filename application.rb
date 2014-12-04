a091010616a075673144a48f82a5bdcaff8a569f
Travis <vasketravisj@johndeere.com>
2014-12-03 20:35:36 -0600

    added all my code for the first time

---
+++ application.rb
@@ -0,0 +1,66 @@
+require "sinatra"
+require "bundler"
+require 'sinatra/json'
+
+Bundler.require
+require "error"
+
+DataMapper.setup(:default, 'sqlite::mmemory:')
+DataMapper.finalize
+DataMapper.auto_migrate!
+
+get "/Error" do
+  "This URL is used for Error Handling"
+end
+
+get    '/ErrorHandler' do
+  content_type :json
+
+ error = Error.all
+ error.to_json
+end
+
+get    '/ErrorHandler/:id' do
+  content_type :json
+
+  errors = Error.get params[:id]
+  errors.to_json
+end
+
+post   '/ErrorHandler' do
+  content_type :json
+
+  error = Error.new params[:error]
+  if error.save
+    status 201
+  else
+    status 500
+    json review.errors.full_messages
+  end
+end
+
+put    '/ErrorHandler/:id' do
+  content_type :json
+
+  error = Error.get params[:id]
+  if error.update params[:error]
+    status 200
+    json "Error was updated"
+  else
+   status 500
+   json error.errors.full_messages
+  end
+end
+  
+delete '/ErrorHandler/:id' do
+  content_type :json
+
+  error = Error.get params[:id]
+  if error.destroy
+    status 200
+    json "Error was removed."
+  else
+    status 500
+    json "There is a problem removing the Error."
+  end
+end
