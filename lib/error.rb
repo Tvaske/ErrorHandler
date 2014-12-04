feddaaee691bd3423327611bfc4c99ffd2102157
Travis <vasketravisj@johndeere.com>
2014-12-03 21:57:46 -0600

    modified error.rb to accept custome errors in table

--- lib/error.rb
+++ lib/error.rb
@@ -2,11 +2,18 @@ class Error
   include DataMapper::Resource
 
   property :id,   Serial
-  property :name, String
-  property :text, String
+  property :server, String
+  property :priority, String
+  property :status, String
+  property :error, String
+  property :errorlong, String
+  property :smgroup, String
   property :created_at, DateTime
-  property :updated_at, DateTime
   
-  validates_presence_of :name
-  validates_presence_of :text
+  validates_presence_of :server
+  validates_presence_of :priority
+  validates_presence_of :status
+  validates_presence_of :error
+  validates_presence_of :errorlong
+  validates_presence_of :smgroup
 end
